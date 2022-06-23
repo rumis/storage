c417228c0699cece1621e9ea581e343d71b78b27
huanjiesm <huanjiesm@163.com>
2022-06-23 10:45:44 +0800

    redis客户端

---
+++ scache/client.go
@@ -0,0 +1,81 @@
+package scache
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Config Redis配置
+type Config struct {
+	Name     string // instance name
+	Addr     string // host:port address.
+	Username string // username
+	Password string // password
+	DB       int    // selected db
+	PoolSize int    // connection pool size, must > 3
+}
+
+var inst *redis.Client // 默认链接
+var servers sync.Map   // 链接池
+
+// DefaultClient 获取默认的客户端
+func DefaultClient() *redis.Client {
+	return inst
+}
+
+// SetDefaultClient 设置默认链接
+// 未加锁，需要程序启动时初始化
+func SetDefaultClient(c *redis.Client) {
+	inst = c
+}
+
+// Get 获取Redis池链接
+func GetClient(name string) (*redis.Client, error) {
+	s, ok := servers.Load(name)
+	if !ok {
+		return nil, fmt.Errorf("server name [%s] not found", name)
+	}
+	c, ok := s.(*redis.Client)
+	if !ok {
+		return nil, fmt.Errorf("server name [%s] is a illegal *redis.Client", name)
+	}
+	return c, nil
+}
+
+// NewClient 初始化
+// 默认会加入连接池
+func NewClient(cfg Config) (*redis.Client, error) {
+	if cfg.PoolSize < 3 {
+		cfg.PoolSize = 3
+	}
+	// init redis client
+	newc := redis.NewClient(&redis.Options{
+		Addr:         cfg.Addr,
+		Username:     cfg.Username,
+		Password:     cfg.Password,
+		DB:           cfg.DB,
+		ReadTimeout:  2 * time.Second, // default read and write timeout is 2s
+		PoolSize:     cfg.PoolSize,
+		MinIdleConns: 3,
+	})
+	res, err := newc.Ping(context.Background()).Result()
+	if err != nil || res != "PONG" {
+		return nil, fmt.Errorf("ping redis [%s] failed, error:%s", cfg.Addr, err.Error())
+	}
+	servers.Store(cfg.Name, newc)
+	return newc, nil
+}
+
+// 创建默认的Redis链接
+func NewDefaultClient(cfg Config) error {
+	defaultClient, err := NewClient(cfg)
+	if err != nil {
+		return err
+	}
+	SetDefaultClient(defaultClient)
+	return nil
+}
--- srepo/seal.go
+++ srepo/seal.go
@@ -1,30 +1,50 @@
 package srepo
 
 import (
-	"database/sql"
-	"fmt"
+	"errors"
+	"sync"
 
 	"github.com/rumis/seal"
-	"github.com/rumis/seal/builder"
-	"github.com/rumis/seal/options"
 )
 
-var sealOpts []options.SealOptionsFunc
+var sealR seal.DB
+var sealW seal.DB
+var sealDBs map[string]seal.DB
+var sealDbOnce sync.Once
 
-// InitSealOptions 初始化Seal相关选项
-func InitSealOptions(fns ...options.SealOptionsFunc) {
-	sealOpts = fns
+// SealR 获取Seal只读实例(从库)
+func SealR() seal.DB {
+	return sealR
 }
 
-// NewSealMysqlDB 创建Seal.Mysql对象
-func NewSealMysqlDB(db *sql.DB) seal.DB {
-	sealDb, err := seal.OpenWithDB(db, builder.NewMysqlBuilder())
-	if err != nil {
-		fmt.Println(err)
+// SealW 获取Seal读写实例(主库)
+func SealW() seal.DB {
+	return sealW
+}
+
+// SetSealR 设置Seal只读实例(从库)
+func SetSealR(db seal.DB) {
+	sealR = db
+}
+
+// SetSealW 设置Seal读写实例(主库)
+func SetSealW(db seal.DB) {
+	sealW = db
+}
+
+// SetSealDB DB池添加实例
+func SetSealDB(inst string, db seal.DB) {
+	sealDbOnce.Do(func() {
+		sealDBs = make(map[string]seal.DB)
+	})
+	sealDBs[inst] = db
+}
+
+// GetSealDB 获取池中DB实例
+func GetSealDB(inst string) (seal.DB, error) {
+	db, ok := sealDBs[inst]
+	if !ok {
+		return seal.DB{}, errors.New("not found")
 	}
-	return sealDb
+	return db, nil
 }
-
-// type SelectClauseHandler func(q *query.SelectQuery)
-
-// type UpdateClauseHandler func(q *query.UpdateQuery)
